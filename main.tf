fb58cb7c46b5f9591c6880143ff5b0f3b3d6b7f2
R S Dileepkumar <dk9901076990@gmail.com>
2025-03-28 00:06:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,7 @@ provider "aws" {
 resource "aws_instance" "test" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
-  count         = 10
-  tags ={
+tags ={
     Name = "test"
   
 }
